01be0a120c7c5735f11e70ce2d427288fd21456f
pablinhoocl <xpabliito@gmail.com>
2023-10-28 22:06:01 -0700

    drag casi listo falta la animacion hover display en proceso, y checkear problemas con el cpushipsetter, ademas de el ataque cpu

--- index.js
+++ index.js
@@ -49,30 +49,77 @@ class Gameboard{
         };
     };
     shipPlacement(length, x, y, orientation){ //falta setear las direcciones etc
+        let spaceChecker= 0;
         const ship= new Ship(length);
+        let boards= document.querySelectorAll('#cpu div');  //ocupar para  buscar los cuadros
         if(orientation=== true){
-            for(let i= 0; i<length; i++){
-                const key = `${[x+ i, y]}`;
-                this.obj.get(key).ships.push(ship);
+            for(let i= 0; i< length; i++){
+                let key = `${[x+ i, y]}`;
+                if(this.obj.get(key).ships.length=== 0){
+                    spaceChecker++;
+                }
+            }
+            if(x+ length> 7 && length!== spaceChecker){
+                alert('error en posicion vuelve a intentarlo')
+            }else if(x+ length< 7 && length=== spaceChecker){
+                for(let i= 0; i<length; i++){
+                    
+                    let key = `${[x+ i, y]}`;
+                    let [xCoord, yCoord] = key.split(',').map(Number);
+                    let coordinate = `${xCoord}, ${yCoord}`;
+                    
+                    let tile= document.getElementById(coordinate)
+                    tile.style.backgroundColor= 'red'
+
+                    console.log(tile)
+                    this.obj.get(key).ships.push(ship);
+                    console.log('nave agregada en: '+`${[x, y]}`)
+                }
             }
         }else{
-            for(let i= 0; i<length; i++){
-                const key = `${[x, y+ i]}`;
-                this.obj.get(key).ships.push(ship);
+            for(let i= 0; i< length; i++){
+                let key = `${[x, y+ i]}`;
+                if(this.obj.get(key).ships.length=== 0){
+                    spaceChecker++;
+                }
+            }
+            if(y+ length> 7 && length!== spaceChecker){
+                alert('error en posicion vuelve a intentarlo')
+            }else if(y+ length< 7 && length=== spaceChecker){
+                for(let i= 0; i<length; i++){
+                    let key = `${[x, y+ i]}`;
+                    let [xCoord, yCoord] = key.split(',').map(Number);
+                    const coordinate = `${xCoord}, ${yCoord}`;
+                     
+                    let tile= document.getElementById(coordinate)
+                    tile.style.backgroundColor= 'red'
+
+                    console.log(tile)
+                    this.obj.get(key).ships.push(ship);
+                    console.log('nave agregada en: '+`${[x, y]}`)
+                }
             }
         }
+        fixCpuBoard();
     }
     receiveAttack(x, y, player, oppo){
         const key= `${x},${y}`
+        const key2= `${x}, ${y}`
         const tile= this.obj.get(key);
         if(tile.ships[0] && !tile.isHit){
             tile.isHit= true;
             tile.ships[0].hit();
+            let selector = `#${player.type} div#${key2}`;
+            let tiles= document.querySelectorAll(selector)
+            tiles.innerText= 'x'
             console.log('hit on '+key)
             changeTurn(player, oppo);
         }else if(tile.ships.length<1 && !tile.isHit){
             tile.isHit= true;
             console.log('missed');
+            let selector = `#${player.type} div#${key2}`;
+            let tiles= document.querySelectorAll(selector)
+            tiles.innerText= 'o'
             changeTurn(player, oppo);
         }
     };
@@ -112,9 +159,9 @@ class Player{
 
 // start game
 
-const startButton= document.getElementById('start');
-startButton.addEventListener('click', function(){
-    startGame();
+const restartButton= document.getElementById('start');
+restartButton.addEventListener('click', function(){
+    restartGame();
 });
 let player1;
 let cpu;
@@ -126,12 +173,14 @@ function startGame(){
     for(let i= 2; i<= 5; i++){
         shipSetterCpu(i, cpu)
     }
+    shipSetterCpu(3, cpu);
     player1.turn= true;
 }
+startGame();
 function boardCreator(player, oppo){
     const mC= document.getElementById('mC');
     const pContainerTiles= document.createElement('div');
-    pContainerTiles.id= player.type;
+    pContainerTiles.id= oppo.type;
     mC.appendChild(pContainerTiles);
     // let player= player;
     // let oppo= oppo
@@ -139,13 +188,13 @@ function boardCreator(player, oppo){
     for (let i = size - 1; i >= 0; i--) {
         for (let j = 0; j < size; j++) {
             const boardTile= document.createElement('div');
-            boardTile.id= i+ ','+ j;
+            boardTile.id= j+ ', '+ i;
             boardTile.classList.add('celdas');
             boardTile.addEventListener('click', function(){
                 let tile1= oppo.board;
                 // .obj.get(`${(i, j)}`);
                 if(player.turn=== true){
-                    tile1.receiveAttack(i, j, player, oppo)
+                    tile1.receiveAttack(j, i, player, oppo)
                 // funcionalidad
                 }
             });
@@ -199,3 +248,57 @@ function shipOrientation(){
 };
 navesButton.addEventListener('click', shipOrientation)
 
+//          DRAG FUNCTION
+
+let draggedShip;
+const shipDocks= Array.from(navesContainer.children);
+shipDocks.forEach(shipDock=> shipDock.addEventListener('dragstart', dragStart));
+const allPlayerBlocks= document.querySelectorAll('#player1 div');
+console.log(allPlayerBlocks)
+allPlayerBlocks.forEach(allPlayerBlock=> {
+   allPlayerBlock.addEventListener('dragover', dragOver);
+    allPlayerBlock.addEventListener('drop', dropShip); 
+})
+
+
+function dragStart(e){
+    draggedShip= e.target.id;
+    console.log(draggedShip);
+}
+
+function dragOver(e){
+    e.preventDefault()
+}
+
+function dropShip(e){
+    let length;
+    let orientation;
+    const startId= e.target.id;
+    console.log(startId);
+    const [x, y] = startId.split(',').map(Number);
+    if(angle=== 90){
+        orientation= true
+    }else if(angle=== 0){
+        orientation= false
+    }
+
+    if(draggedShip==='dos'){
+        length= 2;
+    }else if(draggedShip==='tres'){
+        length= 3;
+    }else if(draggedShip==='cuatro'){
+        length= 4;
+    }else if(draggedShip==='cinco'){
+        length= 5;
+    }
+
+    let command= `${length}, ${startId}, ${orientation}`;
+    console.log(command)
+    player1.board.shipPlacement(length, x, y, orientation);
+}
+function fixCpuBoard(){
+    let cpuBoards= document.querySelectorAll('#cpu div');
+    cpuBoards.forEach(cpuBoard=>{
+        cpuBoard.style.backgroundColor= 'aqua'
+    })
+}
