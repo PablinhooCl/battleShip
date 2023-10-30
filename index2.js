b32d410834d88537ac1e5ee18de1db4a0bcb49b0
pablinhoocl <xpabliito@gmail.com>
2023-10-29 22:47:34 -0700

    solo falta setear el ataque de la cpu y la funcion restart cuando se termina el juego y cuando se empieza el juego, ademas de agregar el display de los turnos

--- index2.js
+++ index2.js
@@ -6,9 +6,6 @@ class Ship{
         this.type= this.typeOfShip(length)
     }
     isSunk(){
-        // while(this.life>0){
-        //     this.current= 'alive'
-        // }
         if(this.life===0){
             this.current= 'SUNKED'
         }
@@ -48,10 +45,9 @@ class Gameboard{
             };
         };
     };
-    shipPlacement(length, x, y, orientation){ //falta setear las direcciones etc
-        
+    shipPlacement(length, x, y, orientation){   //
         const ship= new Ship(length);
-        if(orientation=== true){
+        if(orientation){
             for(let i= 0; i<length; i++){
                 let key = `${[x+ i, y]}`;
                 this.obj.get(key).ships.push(ship);
@@ -67,27 +63,6 @@ class Gameboard{
             }
         }
     }
-    receiveAttack(x, y, player, oppo){
-        const key= `${x},${y}`
-        const key2= `${x}, ${y}`
-        const tile= this.obj.get(key);
-        if(tile.ships[0] && !tile.isHit){
-            tile.isHit= true;
-            tile.ships[0].hit();
-            let selector = `#${player.type} div#${key2}`;
-            let tiles= document.querySelectorAll(selector)
-            tiles.innerText= 'x'
-            console.log('hit on '+key)
-            changeTurn(player, oppo);
-        }else if(tile.ships.length<1 && !tile.isHit){
-            tile.isHit= true;
-            console.log('missed');
-            let selector = `#${player.type} div#${key2}`;
-            let tiles= document.querySelectorAll(selector)
-            tiles.innerText= 'o'
-            changeTurn(player, oppo);
-        }
-    };
     gameDone(){
         let totalCells = 0; 
         let sunkedCells = 0; 
@@ -109,8 +84,6 @@ class Gameboard{
         return false; // keepgoin
     }
 }
-
-
 class Player{
     constructor(type){
         this.board= new Gameboard()
@@ -119,36 +92,13 @@ class Player{
     }
     
 }
-// module.exports = Ship;
-
-
-// start game
-
-const restartButton= document.getElementById('start');
-restartButton.addEventListener('click', function(){
-    restartGame();
-});
-let player1;
+let player;
 let cpu;
-function startGame(){
-    player1= new Player('player');
-    cpu= new Player('cpu');
-    boardCreator(player1, cpu);
-    boardCreator(cpu, player1);
-    for(let i= 2; i<= 5; i++){
-        shipSetterCpu(i, cpu)
-    }
-    shipSetterCpu(3, cpu);
-    player1.turn= true;
-}
-startGame();
 function boardCreator(player, oppo){
     const mC= document.getElementById('mC');
     const pContainerTiles= document.createElement('div');
-    pContainerTiles.id= oppo.type;
+    pContainerTiles.id= player.type;
     mC.appendChild(pContainerTiles);
-    // let player= player;
-    // let oppo= oppo
     let size= 8;
     for (let i = size - 1; i >= 0; i--) {
         for (let j = 0; j < size; j++) {
@@ -156,49 +106,35 @@ function boardCreator(player, oppo){
             boardTile.id= j+ ', '+ i;
             boardTile.classList.add('celdas');
             boardTile.addEventListener('click', function(){
-                let tile1= oppo.board;
-                // .obj.get(`${(i, j)}`);
                 if(player.turn=== true){
-                    tile1.receiveAttack(j, i, player, oppo)
-                // funcionalidad
+                    receiveAttack(j, i, player, oppo)
                 }
             });
+            player.board.obj.get(`${[j, i]}`).tile = boardTile;
             pContainerTiles.appendChild(boardTile);
         }
     }
 };
+function startGame(){
+    player= new Player('player');
+    cpu= new Player('cpu');
+    // const playerBoard= document.getElementById('player')
+    // const cpuBoard= document.getElementById('cpu')       crear funcion restart y cambiar boton de start y funcion por restart
+    // mC.removeChild(playerBoard)
+    // mC.removeChild(cpuBoard)
+    boardCreator(player, cpu);
+    boardCreator(cpu, player);
+    for(let i= 2; i<= 5; i++){
+        shipSetter(i, cpu, null, null, null)
+    }
+    shipSetter(3, cpu, null, null ,null);
+    changeTurn(player, cpu)
+    fixCpuBoard()
+}
+startGame();
 function changeTurn(player, oppo){
     player.turn= false;
     oppo.turn= true 
-}
-function shipSetter(length, player, x, y){
-   let direction= Math.floor(Math.random()*2);
-   if(direction== 0){
-        let boolean= true;
-        let xGo=  (x === null) ? Math.floor(Math.random() * (8 - length)) : x;
-        let yGo=  (y === null) ? Math.floor(Math.random() * (8 - length)) : y;
-        for(let i= xGo; i< xGo+length; i++){
-            let ship= player.board.obj.get(i, yGo);
-            if(ship && ship.ships.length!== 0 && xGo + length>= 8){
-                return shipSetterCpu(length, player)
-            }
-        }
-        console.log(`cpu.board.shipPlacement(${length}, ${xGo}, ${yGo}, ${boolean})`);
-        cpu.board.shipPlacement(length, xGo, yGo, boolean)
-   }else if(direction== 1){
-        let boolean= false;
-        let xGo=  (x === null) ? Math.floor(Math.random() * (8 - length)) : x;
-        let yGo=  (y === null) ? Math.floor(Math.random() * (8 - length)) : y;
-        for(let i= yGo; i< yGo+length; i++){
-            let ship= player.board.obj.get(xGo, i);
-            if(ship && ship.ships.length!== 0 && yGo + length>= 8){
-                return shipSetter(length, player)
-            }
-        }
-        console.log(`cpu.board.shipPlacement(${length}, ${xGo}, ${yGo}, ${boolean})`);
-        player.board.shipPlacement(length, xGo, yGo, boolean)
-    }  
-     
 }
 const navesContainer= document.querySelector('.ships');
 const navesButton= document.getElementById('flipper');
@@ -216,13 +152,13 @@ navesButton.addEventListener('click', shipOrientation)
 let draggedShip;
 const shipDocks= Array.from(navesContainer.children);
 shipDocks.forEach(shipDock=> shipDock.addEventListener('dragstart', dragStart));
-const allPlayerBlocks= document.querySelectorAll('#player1 div');
+const allPlayerBlocks= document.querySelectorAll('#player div');
 console.log(allPlayerBlocks)
 allPlayerBlocks.forEach(allPlayerBlock=> {
    allPlayerBlock.addEventListener('dragover', dragOver);
     allPlayerBlock.addEventListener('drop', dropShip); 
 })
-s
+
 
 function dragStart(e){
     draggedShip= e.target.id;
@@ -237,12 +173,12 @@ function dropShip(e){
     let length;
     let orientation;
     const startId= e.target.id;
-    console.log(startId);
+    const naveDiv= document.getElementById(draggedShip)
     const [x, y] = startId.split(',').map(Number);
     if(angle=== 90){
-        orientation= true
+        orientation= 1
     }else if(angle=== 0){
-        orientation= false
+        orientation= 0
     }
 
     if(draggedShip==='dos'){
@@ -254,10 +190,12 @@ function dropShip(e){
     }else if(draggedShip==='cinco'){
         length= 5;
     }
-
-    let command= `${length}, ${startId}, ${orientation}`;
-    console.log(command)
-    player1.board.shipPlacement(length, x, y, orientation);
+    console.log(naveDiv)
+    const placedSuccessfully = shipSetter(length, player, x, y, orientation);
+    if (placedSuccessfully) {
+        navesContainer.removeChild(naveDiv);
+    }
+    
 }
 function fixCpuBoard(){
     let cpuBoards= document.querySelectorAll('#cpu div');
@@ -265,3 +203,71 @@ function fixCpuBoard(){
         cpuBoard.style.backgroundColor= 'aqua'
     })
 }
+function shipSetter(length, play, x, y, direction){
+    let directionXY= (direction!= 0 && direction!=1) ? Math.floor(Math.random()*2): direction
+    console.log(x, y)
+    if(directionXY== 1){
+        let boolean= true;
+        let xGo=  (x === null) ? Math.floor(Math.random() * (8 - length)) : x;
+        let yGo=  (y === null) ? Math.floor(Math.random() * (8 - length)) : y;
+        console.log(xGo, yGo)
+        for(let i= xGo; i< xGo+length; i++){
+            let key= `${i},${yGo}`;
+            let ship= play.board.obj.get(key);
+            console.log(ship)
+            if(ship.ships.length!= 0 || xGo + length> 8){ // borre ship &&
+                if(play=== player){
+                    return
+                }else{
+                    return shipSetter(length, play, null, null, null)
+                }
+            }
+        }
+        play.board.shipPlacement(length, xGo, yGo, boolean)
+        return true
+   }else if(directionXY== 0){
+        let boolean= false;
+        let xGo=  (x === null) ? Math.floor(Math.random() * (8 - length)) : x;
+        let yGo=  (y === null) ? Math.floor(Math.random() * (8 - length)) : y;
+        console.log(xGo, yGo)
+
+        for(let i= yGo; i< yGo+length; i++){
+            let key= `${xGo},${i}`;
+
+            let ship= play.board.obj.get(key);
+            console.log(ship)
+
+            if(ship.ships.length!== 0 || yGo + length> 8){ // borre ship && 
+                if (play === player) {
+                    return false;
+                } else {
+                    return shipSetter(length, play, null, null, null);
+                }
+            }
+        }
+        play.board.shipPlacement(length, xGo, yGo, boolean)
+        return true
+   }
+}
+function receiveAttack(x, y, player, oppo){
+    const key= `${x},${y}`
+    const square= player.board.obj.get(key);
+    const shipContainer= document.getElementsByClassName('ships')
+    if(shipContainer[0].childElementCount== 0){
+    if(square.ships[0] && !square.isHit){
+        square.isHit= true;
+        square.ships[0].hit();
+        square.tile.innerText= 'x'
+        console.log('hit on '+key)
+        changeTurn(player, oppo);
+    }else if(square.ships.length<1 && !square.isHit){
+        square.isHit= true;
+        console.log('missed');
+        square.tile.innerText= 'o'
+        changeTurn(player, oppo);
+    }}
+}
+const startButton= document.getElementById('start');
+startButton.addEventListener('click', function(){
+    startGame();
+});
